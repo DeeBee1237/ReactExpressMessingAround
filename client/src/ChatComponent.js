d6d7a3d4230c3e86a94baa61bb4b224b30f74108
Dragos <dragosbercea@gmail.com>
2025-03-15 16:08:05 +1300

    got the messages around the right way this time

--- client/src/ChatComponent.js
+++ client/src/ChatComponent.js
@@ -3,8 +3,10 @@ import { useParams } from 'react-router-dom';
 
 function ChatComponent(props) {
 
-  const [messagesSent, setMessagesSent] = useState([]);
-  const [messagesRcvd, setMessagesRcvd] = useState([]);
+  const [allMessages, setAllMessages] = useState([]);
+
+  // const [messagesSent, setMessagesSent] = useState([]);
+  // const [messagesRcvd, setMessagesRcvd] = useState([]);
 
   // the user name for the chat
   const { name } = useParams();
@@ -16,20 +18,22 @@ function ChatComponent(props) {
       .then(data => {
         
         // TODO: this relies on users without duplicate names CHECK FOR THIS!
-        let sentMessageObjs = data.messages.filter((messageObj) => messageObj.userName === name)
-        let sentMessages = sentMessageObjs.map((messageObj) => messageObj.message)
+        // let sentMessageObjs = data.messages.filter((messageObj) => messageObj.userName === name)
+        // let sentMessages = sentMessageObjs.map((messageObj) => messageObj.message)
 
-        let rcvdMessageObjs = data.messages.filter((messageObj) => messageObj.userName !== name)
-        let rcvdMessages = rcvdMessageObjs.map((messageObj) => messageObj.message)
+        // let rcvdMessageObjs = data.messages.filter((messageObj) => messageObj.userName !== name)
+        // let rcvdMessages = rcvdMessageObjs.map((messageObj) => messageObj.message)
         
-        // .map((messageObj) => messageObj.message)
-        // let actualMessages2 = data.messages.map((messageObj) => messageObj.message)
+        // // .map((messageObj) => messageObj.message)
+        // // let actualMessages2 = data.messages.map((messageObj) => messageObj.message)
         
-        setMessagesSent(sentMessages)
-        setMessagesRcvd(rcvdMessages)
+        // setMessagesSent(sentMessages)
+        // setMessagesRcvd(rcvdMessages)
+
+        setAllMessages(data.messages)
       })
 
-  }, [messagesSent])
+  }, [allMessages]) //messagesSent])
 
   function search(formData) {
     const message = formData.get("newChatMessage");
@@ -51,17 +55,19 @@ function ChatComponent(props) {
       .then((data) => {
         // let actualMessages = data.messages.map((messageObj) => messageObj.message)
         // setMessages(actualMessages);
-        let sentMessageObjs = data.messages.filter((messageObj) => messageObj.userName === name)
-        let sentMessages = sentMessageObjs.map((messageObj) => messageObj.message)
+        // let sentMessageObjs = data.messages.filter((messageObj) => messageObj.userName === name)
+        // let sentMessages = sentMessageObjs.map((messageObj) => messageObj.message)
 
-        let rcvdMessageObjs = data.messages.filter((messageObj) => messageObj.userName !== name)
-        let rcvdMessages = rcvdMessageObjs.map((messageObj) => messageObj.message)
+        // let rcvdMessageObjs = data.messages.filter((messageObj) => messageObj.userName !== name)
+        // let rcvdMessages = rcvdMessageObjs.map((messageObj) => messageObj.message)
         
-        // .map((messageObj) => messageObj.message)
-        // let actualMessages2 = data.messages.map((messageObj) => messageObj.message)
+        // // .map((messageObj) => messageObj.message)
+        // // let actualMessages2 = data.messages.map((messageObj) => messageObj.message)
         
-        setMessagesSent(sentMessages)
-        setMessagesRcvd(rcvdMessages)
+        // setMessagesSent(sentMessages)
+        // setMessagesRcvd(rcvdMessages)
+
+        setAllMessages(data.messages)
       });
   }
 
@@ -71,13 +77,20 @@ function ChatComponent(props) {
 
       {
         <div class="chat">
+
+        {allMessages.map((messageObj, i) => (
+          <div data-time="16:35" class={messageObj.userName === name ? "msg sent" : "msg rcvd"}>{messageObj.message}</div>
+        ))}
+        
+        {/* 
          {messagesSent.map((message, i) => (
           <div data-time="16:35" class="msg sent">{message}</div>
         ))}
 
         {messagesRcvd.map((message, i) => (
           <div data-time="16:35" class="msg rcvd">{message}</div>
-        ))}
+        ))} */}
+        
         </div>
       }
 
